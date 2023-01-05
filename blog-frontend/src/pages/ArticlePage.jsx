928f3126933611ab13c2b4daca54faadce723d4f
george ogolla <georgyogolla@gmail.com>
2023-01-05 11:50:38 +0300

    added proxy to run both server and client on the same port

--- blog-frontend/src/pages/ArticlePage.jsx
+++ blog-frontend/src/pages/ArticlePage.jsx
@@ -1,9 +1,23 @@
-import { useParams } from "react-router-dom"; //
-import articles from "./articles-content"; //importing the articles from the article-content.js file
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom"; //useParams is a hook that allows us to access the parameters of the current route
+import axios from "axios";
+import articles from "./articles-content";
 import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage = () => {
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
   const { articleId } = useParams();
+
+  useEffect(() => {
+    const loadArticle = async () => {
+      const response = await axios.get(`/api/articles/${articleId}`);
+      const newArticleInfo = response.data;
+      setArticleInfo(newArticleInfo);
+    };
+
+    loadArticle();
+  }, []);
+
   // const {articleId} = params; //destructruing object parameters
   // const {aticleId} = params.articleId;//using full object notation to acces the id of parameters
   const article = articles.find((article) => article.name === articleId);
@@ -14,6 +28,7 @@ const ArticlePage = () => {
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This article has {articleInfo.upvotes} upvote(s)</p>
       {article.content.map((paragraph, i) => (
         <p key={i}>{paragraph}</p>
       ))}
