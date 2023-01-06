4687dcaef57d3c4632300a811748dcc09dde7d1c
george ogolla <georgyogolla@gmail.com>
2023-01-06 09:51:23 +0300

    implemented upvote and add comment features

--- blog-frontend/src/pages/ArticlePage.jsx
+++ blog-frontend/src/pages/ArticlePage.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom"; //useParams is a hook that allows us to access the parameters of the current route
 import axios from "axios";
-import articles from "./articles-content";
+import CommentsList from "../components/CommentsLits";
+import AddCommentForm from "../components/AddCommentForm";
 import NotFoundPage from "./NotFoundPage";
+import articles from "./articles-content";
 
 const ArticlePage = () => {
   const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
@@ -25,13 +27,29 @@ const ArticlePage = () => {
   if (!article) {
     return <NotFoundPage />;
   }
+
+  const addUpvote = async () => {
+    const esponse = await axios.put(`/api/articles/${articleId}/upvote`);
+    const updatedArticleInfo = response.data;
+    setArticleInfo(updatedArticleInfo);
+  };
   return (
     <>
       <h1>{article.title}</h1>
-      <p>This article has {articleInfo.upvotes} upvote(s)</p>
+
+      <div className="upvotes-section">
+        <button onClick={addUpvote}>Upvote</button>
+        <p>This article has {articleInfo.upvotes} upvote(s)</p>
+      </div>
+
       {article.content.map((paragraph, i) => (
         <p key={i}>{paragraph}</p>
       ))}
+      <AddCommentForm
+        articleName={articleId}
+        onArticleUpdate={(updatedArticle) => setArticleInfo(updatedArticle)}
+      />
+      <CommentsList comments={articleInfo.comments} />
     </>
   );
 };
